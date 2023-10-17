70d6f688cb37cff49024546bdf4d582da064dd12
Giulio <3272563+giuliohome@users.noreply.github.com>
2023-10-17 09:31:43 +0200

    Standard test (campaign ended)

--- tests/enicom.spec.ts
+++ tests/enicom.spec.ts
@@ -2,8 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test Just Transition', async ({ page }, testInfo) => {
   await page.goto('https://www.eni.com/en-IT/home.html');
-  await page.locator('#ModalPageCampaign img').click();
-  await page.locator('#ModalPageCampaign').getByLabel('Close').click();
+  // await page.locator('#ModalPageCampaign').getByLabel('Close').click();
   await page.getByRole('link', { name: 'Just Transition' }).click();
   await expect(page.getByRole('heading', { name: 'Just Transition', exact: true })).toBeVisible();
   const screenshot = await page.screenshot();
