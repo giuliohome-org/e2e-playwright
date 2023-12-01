09bfe0482f0165f98f2d167a882fdafc2d11fc0d
Giulio <3272563+giuliohome@users.noreply.github.com>
2023-12-01 23:02:06 +0100

    sustainable energy

--- tests/enicom.spec.ts
+++ tests/enicom.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('test Just Transition', async ({ page }, testInfo) => {
+test('test Sustainable Energy', async ({ page }, testInfo) => {
   await page.goto('https://www.eni.com/en-IT/home.html');
-  await page.locator('#ModalPageCampaign').getByLabel('Close').click();
-  await page.getByRole('link', { name: 'Just Transition' }).click();
-  await expect(page.getByRole('heading', { name: 'Just Transition', exact: true })).toBeVisible();
+  await page.getByRole('button', { name: 'Vision' }).click();
+  await page.locator('li').filter({ hasText: 'Accessible energy' }).nth(1).click();
+  await page.locator('#menuHeader').getByRole('link', { name: 'Accessible energy' }).click();
+  await expect(page.locator('#widget--1817102624')).toContainText('We support a transition that gives everyone access to reliable and sustainable energy');
   const screenshot = await page.screenshot();
-  // await page.waitForTimeout(3000);
   await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.2 });
-  await testInfo.attach('Just Transition', {
+  await testInfo.attach('Sustainable Energy', {
     body: screenshot,
     contentType: 'img/png'
   });
